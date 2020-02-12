47f0834087932ef491800d9e9811235a0a076628
sdz248 <60815563+sdz248@users.noreply.github.com>
2020-02-12 11:46:14 -0300

    Switch

---
+++ jsInstruccionSwitch(1ok).js
@@ -0,0 +1,39 @@
+function mostrar()
+{
+//tomo la edad  
+var mesDelAnio = document.getElementById('mes').value;
+
+switch(mesDelAnio) {
+
+case "Enero" :
+     alert("Que comiences bien el año");
+     break;
+
+
+case "Marzo" :
+  alert("A clases");
+  break;
+
+  case "Julio" :
+  alert("Se vienen las vacaciones");
+  break;
+
+
+
+  case "Diciembre" :
+  alert("Felices fiestas");
+  break;
+
+
+
+
+
+
+
+
+}
+
+ 
+
+
+}//FIN DE LA FUNCIÓN
